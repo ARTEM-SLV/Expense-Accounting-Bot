7fe5040017f797a809de6464eb3d33e025ce1cca
Artem <67641352+ARTEM-SLV@users.noreply.github.com>
2024-11-12 17:08:29 +0300

    v1.0.20

--- internal/utils/logger/logger.go
+++ internal/utils/logger/logger.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 // Logger структура для логирования в файл
 type Logger struct {
 	file *os.File
+	mu   sync.Mutex
 }
 
 var L *Logger
@@ -28,31 +30,47 @@ func InitLogger(filepath string) error {
 }
 
 // Info записывает информационное сообщение
-func (l *Logger) Info(message string) {
-	log.Println("INFO: ", message)
+func (l *Logger) Info(msg string) {
+	go func() {
+		l.printLog("INFO: ", msg)
+	}()
 }
 
 // Warning записывает сообщение об ошибке
-func (l *Logger) Warning(message string) {
-	log.Println("WARNING: ", message)
+func (l *Logger) Warning(msg string) {
+	go func() {
+		l.printLog("WARNING: ", msg)
+	}()
 }
 
 // Error записывает сообщение об ошибке
 func (l *Logger) Error(message string, err error) {
 	msg := fmt.Sprintf("%s %v", message, err)
-	log.Println("ERROR: ", msg)
+	go func() {
+		l.printLog("ERROR: ", msg)
+	}()
 }
 
 // Error ошибка отправки сообщение ботом
 func (l *Logger) ErrorSendMessage(err error) {
 	msg := fmt.Sprintf("Не удалось отправить сообщение %v", err)
-	log.Println("ERROR: ", msg)
+	go func() {
+		l.printLog("ERROR: ", msg)
+	}()
 }
 
 // Error ошибка изменения сообщение ботом
 func (l *Logger) ErrorEditMessage(err error) {
 	msg := fmt.Sprintf("Не удалось изменить сообщение %v", err)
-	log.Println("ERROR: ", msg)
+	go func() {
+		l.printLog("ERROR: ", msg)
+	}()
+}
+
+func (l *Logger) printLog(grade string, msg string) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	log.Println(grade, msg)
 }
 
 // Close закрывает файл логов
